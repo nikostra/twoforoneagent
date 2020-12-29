c522e7c0b9cffcf5d0e307746c5880c7eda120e9
Niko <e11810855@student.tuwien.ac.at>
2020-12-29 13:20:17 +0100

    init

---
+++ src/main/java/at/pwd/twoforoneagent/TwoForOneAgent.java
@@ -0,0 +1,14 @@
+package at.pwd.twoforoneagent;
+
+
+import at.pwd.boardgame.game.mancala.MancalaGame;
+import at.pwd.boardgame.game.mancala.agent.MancalaAgent;
+import at.pwd.boardgame.game.mancala.agent.MancalaAgentAction;
+
+public class TwoForOneAgent implements MancalaAgent {
+
+    @Override
+    public MancalaAgentAction doTurn(int computationTime, MancalaGame mancalaGame){
+        return null;
+    }
+}
